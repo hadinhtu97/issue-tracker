0725db7155f4b724d32c81cdf13c347b43f85b60
Tu Ha Dinh <hadinhtu97@gmail.com>
2020-12-19 01:25:22 +0700

    update

--- server.js
+++ server.js
@@ -1,27 +1,21 @@
 'use strict';
 
-// require modules
 const express = require("express");
 const cors = require('cors');
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
-// require ruotes
 const apiRoutes = require('./routes/api.js');
 
 const app = express();
 
-// for fcc testing 
 app.use(cors({ optionsSuccessStatus: 200 }));
 
-// Parse incoming request bodies in a middleware before your handlers
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// send css and js to client
 app.use(express.static(__dirname + "/public"));
 
-// send home index
 app.route('/').get((req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
